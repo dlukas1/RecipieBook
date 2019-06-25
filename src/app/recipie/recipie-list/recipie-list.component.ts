faef05c554f976cf02723970efcda97cb462fe39
Dmitry Lukas <dmitry.lukas@helmes.com>
2019-06-25 20:20:55 +0300

    6-3 can show recipies list

---
+++ src/app/recipie/recipie-list/recipie-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipie } from '../recipie.model';
+
+@Component({
+  selector: 'app-recipie-list',
+  templateUrl: './recipie-list.component.html',
+  styleUrls: ['./recipie-list.component.css']
+})
+export class RecipieListComponent implements OnInit {
+
+  recipies: Recipie[] = [
+    new Recipie('A test recipie', 'Sample test', 'https://hips.hearstapps.com/del.h-cdn.co/assets/18/11/2048x1024/landscape-1520957481-grilled-salmon-horizontal.jpg?resize=1200:*'),
+    new Recipie('A test recipie', 'Sample test', 'https://hips.hearstapps.com/del.h-cdn.co/assets/18/11/2048x1024/landscape-1520957481-grilled-salmon-horizontal.jpg?resize=1200:*')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
